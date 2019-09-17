77ecb7ae112b5a04468b1f9978504294ed2f868d
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-17 05:39:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,5 @@
 class Dog
   
-  def initialize(name)
-    @name = name
-  end
-  
-  #getter
-  def name
-    @name
-  end
-  
-  #setter
-  def name=(new_name)
-    @name = new_name
-  end
+
   
 end
